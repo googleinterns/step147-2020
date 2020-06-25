bf7852cf96e53ab95a762c0f79eda5b161cb25bb
Mohamed <mhmshatry@gmail.com>
2020-06-25 02:09:59 +0000

    Define dummy variables

--- src/main/ngapp/src/app/app.component.ts
+++ src/main/ngapp/src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  title = 'angular-chat';
+  channel: ""
+  username = '';
+  messages: ["Message1"]
+  newMessage = '';
+  channelList: [];
+  chatClient: "";
+  currentUser: "User";
+
 }
