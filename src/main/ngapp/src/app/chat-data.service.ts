a7bbb265d10cb7593a980a8984caf7e67f244804
mhmshatry@gmail.com <mhmshatry@gmail.com>
2020-07-15 16:10:57 +0000

    Update chat data service to have better dataflow

--- src/main/ngapp/src/app/chat-data.service.ts
+++ src/main/ngapp/src/app/chat-data.service.ts
@@ -18,14 +18,7 @@ export class ChatDataService {
   pusher: any;
   channel: any;
 
-  requestChannel: string = "";
-
-  constructor(private authService: AuthService, private http: HttpClient ) {
-    this.pusher = new Pusher(environment.pusher.key, {
-        cluster: environment.pusher.cluster,
-        encrypted: true
-    });
-  }
+  constructor(private authService: AuthService, private http: HttpClient ) {} 
 
   // Get the object associated with the currrent user.
   getUser(): Observable<User[]> {
@@ -37,42 +30,56 @@ export class ChatDataService {
 
   // Create a new user object for the user.
   addUser(user: User): void{
-      this.http.post("/user", user);
+    this.http.post("/user", user);
   }
 
+  // Get a list of all the users in our service.
   getUsers(): Observable<User[]> {
     var localUser = JSON.parse(localStorage.getItem("user"));
     return this.http.get<User[]>("/users?userId="+localUser.uid);
   }
 
+  // Get messages between two users.
   getMessages(recipient: string): Observable<Message[]> {
     var localUser = JSON.parse(localStorage.getItem("user"));
-    let url: string = "/chatroom?userId=" + localUser.uid + "&recipientId=" + recipient;
+    const url: string = "/chatroom?userId=" + localUser.uid + "&recipientId=" + recipient;
     return this.http.get<Message[]>(url);
   }
 
+  // Get a chatroom for a recepient;
   getChatroom(recipient: string): string {
     let chat: string = "";
     var localUser = JSON.parse(localStorage.getItem("user"));
-    let url: string = "/getChatroom?userId=" + localUser.uid + "&recipientId=" + recipient;
+    const url: string = "/getChatroom?userId=" + localUser.uid + "&recipientId=" + recipient;
     
     const promise = this.http.get<Chatroom>(url).toPromise();
     promise.then(res => {
         console.log("Chatroom Id: ", res[0].chatroomId);
-        this.requestChannel = res[0].chatroomId;
-        chat = this.requestChannel;
-        this.channel = this.pusher.subscribe(this.requestChannel);
+        chat = res[0].chatroomId;
+        this.setPusher(chat);
+        return chat;
     }).catch(err => console.error(err));
 
     return chat;
   }
- 
-  addMessage(input: Post){
-    console.log("about to send post", input);
-    const promise = this.http.post<Post>("/chatroom", input).toPromise();
-    promise.then(res => console.log("Res: ", res)).catch(err => console.error(err));
+
+  // Set pusher to new channel.
+  setPusher(chatroom: string){
+    this.pusher = new Pusher(environment.pusher.key, {
+        cluster: environment.pusher.cluster,
+        encrypted: true
+    });
+    this.channel = this.pusher.subscribe(chatroom);
   }
  
+  // Add message in a new chatroom.
+  addMessage(input: Post){
+    const promise = this.http.post<Post>("/chatroom", input).toPromise();
+    promise.catch(err => console.error("Error sending new message: ", err));
+
+  }
+ 
+  // Logout of the service.
   logout(){
     this.authService.logout();
   }
