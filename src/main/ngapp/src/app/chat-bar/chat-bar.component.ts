eba41a63b4d06917470c1a4de059057c7df380d0
mhmshatry@gmail.com <mhmshatry@gmail.com>
2020-07-20 15:15:52 +0000

    Remove old theming from chat bar

--- src/main/ngapp/src/app/chat-bar/chat-bar.component.ts
+++ src/main/ngapp/src/app/chat-bar/chat-bar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { User } from '../models/user';
-import { Chatroom } from '../models/chatroom';
 
 
 @Component({
@@ -11,22 +10,12 @@ import { Chatroom } from '../models/chatroom';
 export class ChatBarComponent implements OnInit {
   
   @Output() changeChat: EventEmitter<any> = new EventEmitter();
-
   @Input() users: User[];
-  @Input() chatrooms: Chatroom[];
-
-  currId: string;
-  chatroomUsers : any[];
 
   constructor() { }
 
   ngOnInit(): void {
-    const localUser = JSON.parse(localStorage.getItem("user"));
-    this.currId = localUser.uid;
-    console.log("Chatroom Users: ", this.chatroomUsers);
   }
-
-  
 
   onSelect(id: string){
       this.changeChat.emit(id);
