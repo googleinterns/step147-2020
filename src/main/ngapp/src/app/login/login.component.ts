085eeadb4e38de709a0e4fe7ef481eef84eb2d6a
mhmshatry@gmail.com <mhmshatry@gmail.com>
2020-07-21 14:36:15 +0000

    Update login component to use nebular theme

--- src/main/ngapp/src/app/login/login.component.ts
+++ src/main/ngapp/src/app/login/login.component.ts
@@ -9,15 +9,47 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   
+  loginAwait = false;
+  error: any;
+  errorPresent: boolean = false;
   constructor(public authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
   login(email: string, password: string): void {
-    this.authService.login(email, password);
+    this.loginAwait = true;
+    this.authService.login(email, password).then((res: any) => {
+        localStorage.setItem('user', JSON.stringify(res.user));
+        this.loginAwait = false;
+        this.router.navigate(['/chat']);
+    })
+    .catch((error: any) => {
+        this.loginAwait = false;
+        this.errorPresent = true;
+        this.error = error.message;
+    });
+  }
+
+  onClose(){
+      this.errorPresent = false;
   }
 
   sendPasswordResetEmail(email: string) {
     this.authService.sendPasswordResetEmail(email);
   }
+
+  loginWithGoogle(){
+      this.authService.googleAuth().catch((error: any) => {
+        this.errorPresent = true;
+        this.error = error.message;
+    });
+  }
+
+  loginWithFacebook(){
+      this.authService.googleAuth().catch((error: any) => {
+        this.errorPresent = true;
+        this.error = error.message;
+    });
+  }
+
 }
