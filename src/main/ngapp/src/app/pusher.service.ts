0a35da4ae07231b3fde91a852c639956404c0326
MohamedShatry <mhmshatry@gmail.com>
2020-07-22 15:20:54 -0400

    Update pusher service to include unsubscribe functionality

--- src/main/ngapp/src/app/pusher.service.ts
+++ src/main/ngapp/src/app/pusher.service.ts
@@ -4,19 +4,26 @@ import { environment } from '../environments/environment';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PusherService {
-    pusher: any;
-    channel: any;
-    constructor(private http: HttpClient) {}
+  pusher: any;
+  channel: any;
+  currentChannel: string = '';
 
-    setPusher(channel : string): void {
-        this.pusher = new Pusher(environment.pusher.key, {
-        cluster: environment.pusher.cluster,
-        encrypted: true
-        });
-        this.channel = this.pusher.subscribe(channel);
+  constructor(private http: HttpClient) {
+    this.pusher = new Pusher(environment.pusher.key, {
+      cluster: environment.pusher.cluster,
+      encrypted: true,
+    });
+  }
 
-    }
+  setPusher(channel: string): void {
+    this.currentChannel = channel;
+    this.channel = this.pusher.subscribe(channel);
+  }
+
+  unsubscribePusher() {
+    this.pusher.unsubscribe(this.currentChannel);
+  }
 }
