eee138db9997703f0700252c05dab8b36ef0a6a7
Kenneth Kiprotich <kiprotich@google.com>
2020-07-23 20:34:59 +0000

    add func to show how firebase servlet works in backend

--- src/main/ngapp/src/app/login/login.component.ts
+++ src/main/ngapp/src/app/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+import * as firebase from 'firebase/app';
+
 
 @Component({
   selector: 'app-login',
@@ -10,8 +15,8 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   loginAwait = false;
   error: any;
-  errorPresent = false;
-  constructor(public authService: AuthService, private router: Router) {}
+  errorPresent: boolean = false;
+  constructor(public authService: AuthService, private router: Router, public http: HttpClient) {}
 
   ngOnInit(): void {}
 
@@ -23,8 +28,12 @@ export class LoginComponent implements OnInit {
         localStorage.setItem('user', JSON.stringify(res.user));
         this.loginAwait = false;
         this.router.navigate(['/chat']);
-      })
-      .catch((error: any) => {
+    }).then (() => {
+
+        //CALL TO VERIFYIDTOKEN IS HERE.
+        this.verifyIdToken();
+    }) 
+    .catch((error: any) => {
         this.loginAwait = false;
         this.errorPresent = true;
         this.error = error.message;
@@ -70,4 +79,34 @@ export class LoginComponent implements OnInit {
         this.error = error.message;
       });
   }
+
+  // Sends idToken to Firebase in the backend.
+  verifyIdToken() {
+    // Get idToken from firebase.
+    firebase.auth().currentUser.getIdToken()
+    .then((idToken) => {
+        console.log("this is the login currentuser idToken", idToken);
+        var token = JSON.stringify(idToken);
+
+        // Add the idToken as a custom header.
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+                'X-token': idToken
+            }),
+        };
+        console.log('httpOptions: ', httpOptions);
+       
+        // Send token to your backend via HTTPS.
+        const tokenRes = this.http.get("/idTokenVerification", httpOptions).toPromise()
+        .then((result) => {
+            console.log('this is result from backend', JSON.stringify(result));
+        });
+    })
+    .catch( (error) => {
+        console.log("this is an error from idToken: ",error.message);
+    })
+    
+  }
+
 }
