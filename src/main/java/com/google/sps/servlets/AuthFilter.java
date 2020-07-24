5ccd1519494fe2fe834f99da37dbed4014dc9e85
Kenneth Kiprotich <kiprotich@google.com>
2020-07-24 14:56:06 +0000

    change a few bugs in filter

--- src/main/java/com/google/sps/servlets/AuthFilter.java
+++ src/main/java/com/google/sps/servlets/AuthFilter.java
@@ -1,14 +1,27 @@
+// Copyright 2017 Google Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
 import javax.servlet.FilterChain;
 import javax.servlet.Filter;
 import javax.servlet.FilterConfig;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebInitParam;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.ServletException;
-import java.io.IOException;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.FirebaseOptions;
@@ -25,9 +38,10 @@ import java.io.*;
 import java.util.List;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.auth.FirebaseAuthException;
-import java.io.*;
+import com.google.sps.servlets.MutableHttpServletRequest;
 
-@WebFilter(urlPatterns= {"/user", "/users", "/getChatroom", "messages"})
+
+@WebFilter(urlPatterns= {"/chatrooms", "/user", "/users", "/getChatroom", "/messages"})
 public class AuthFilter implements Filter {
 
     private FirebaseToken decodedToken;
@@ -37,7 +51,9 @@ public class AuthFilter implements Filter {
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         
-        FileInputStream serviceAccount = new FileInputStream("/home/dez/step/step147-2020/src/main/webapp/WEB-INF/node.json");
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        InputStream serviceAccount = FirebaseAuthVerification.class.getResourceAsStream("/firebaseServiceAccount.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -57,12 +73,12 @@ public class AuthFilter implements Filter {
             firebaseApp = FirebaseApp.initializeApp(options);   
         }
 
-        System.out.println("\n\n\n\n\n" + firebaseApp + "\n\n\n\n");
+        System.out.println("\n\n\n this is firebaseapp instance: " + firebaseApp + "\n\n\n");
 
         // idToken comes from the Frontend as a parameter.
-        String idToken = request.getParameter("auth-token");
+        String idToken = req.getHeader("X-token");
 
-        System.out.println("\n\n\n\n\n" + idToken + "\n\n\n\n");
+        System.out.println("\n\n\n this is idtoken: " + idToken + "\n\n\n");
 
         try {
             decodedUseridToken = FirebaseAuth.getInstance().verifyIdToken(idToken);;
@@ -73,12 +89,12 @@ public class AuthFilter implements Filter {
         String uid = decodedUseridToken.getUid();
         Gson gson = new Gson();
 
-        HttpServletRequest req = (HttpServletRequest) request;
         MutableHttpServletRequest mutableRequest = new MutableHttpServletRequest(req);
 
         mutableRequest.putHeader("userId", uid);
         chain.doFilter(mutableRequest, response);
+        System.out.println("\n\n\n we reached here in AuthFilter\n\n\n");
 
-        System.out.println(uid);
+        System.out.println("\n\n\nuid from authfilter", + uid + "\n\n\n");
     }
 }
