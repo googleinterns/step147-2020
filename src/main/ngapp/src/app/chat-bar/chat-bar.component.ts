18ec7aac92da0e0ad1d97083c58e741b6eea850f
mhmshatry@gmail.com <mhmshatry@gmail.com>
2020-07-28 18:48:15 +0000

    Update input in chat bar component

--- src/main/ngapp/src/app/chat-bar/chat-bar.component.ts
+++ src/main/ngapp/src/app/chat-bar/chat-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { User } from '../models/user';
+import { Chat } from '../models/chat';
+import { Message } from '../models/message';
 import { Chatroom } from '../models/chatroom';
 
 @Component({
@@ -7,10 +9,10 @@ import { Chatroom } from '../models/chatroom';
   templateUrl: './chat-bar.component.html',
   styleUrls: ['./chat-bar.component.css'],
 })
-export class ChatBarComponent implements OnInit {
-  @Output() changeChat: EventEmitter<User> = new EventEmitter();
 
-  @Input() users: User[];
+export class ChatBarComponent implements OnInit {
+  @Output() changeChat: EventEmitter<Chat> = new EventEmitter();
+  @Input() users: Chat[];
   currId: string;
 
   constructor() {}
@@ -20,7 +22,19 @@ export class ChatBarComponent implements OnInit {
     this.currId = localUser.uid;
   }
 
-  onSelect(user: User) {
-    this.changeChat.emit(user);
+  getLastMessage(messages: Message[]): string {
+    let message: string;
+
+    if(this.currId === messages[messages.length - 1].senderId){
+        message = messages[messages.length - 1].text;
+    }else{
+        message = messages[messages.length - 1].translatedText;
+    }
+
+    return message;
+  }
+
+  onSelect(chat: Chat) {
+    this.changeChat.emit(chat);
   }
 }
