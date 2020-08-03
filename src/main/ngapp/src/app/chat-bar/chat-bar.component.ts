a281d119819f440b218ba3324a0893844f1ecb37
mhmshatry@gmail.com <mhmshatry@gmail.com>
2020-08-03 20:11:44 +0000

    Update nested if statements

--- src/main/ngapp/src/app/chat-bar/chat-bar.component.ts
+++ src/main/ngapp/src/app/chat-bar/chat-bar.component.ts
@@ -3,38 +3,38 @@ import { User } from '../models/user';
 import { Chat } from '../models/chat';
 import { Message } from '../models/message';
 import { Chatroom } from '../models/chatroom';
-
+ 
 @Component({
-  selector: 'app-chat-bar',
-  templateUrl: './chat-bar.component.html',
-  styleUrls: ['./chat-bar.component.css'],
+ selector: 'app-chat-bar',
+ templateUrl: './chat-bar.component.html',
+ styleUrls: ['./chat-bar.component.css'],
 })
-
 export class ChatBarComponent implements OnInit {
-  @Output() changeChat: EventEmitter<Chat> = new EventEmitter();
-  @Input() users: Chat[];
-  currId: string;
-
-  constructor() {}
-
-  ngOnInit(): void {
-    const localUser = JSON.parse(localStorage.getItem('user'));
-    this.currId = localUser.uid;
-  }
-
-  getLastMessage(messages: Message[]): string {
-    let message: string;
-
-    if(this.currId === messages[messages.length - 1].senderId){
-        message = messages[messages.length - 1].text;
-    }else{
-        message = messages[messages.length - 1].translatedText;
-    }
-
-    return message;
-  }
-
-  onSelect(chat: Chat) {
-    this.changeChat.emit(chat);
-  }
+ @Output() changeChat: EventEmitter<Chat> = new EventEmitter();
+ @Input() users: Chat[];
+ currId: string;
+ 
+ constructor() {}
+ 
+ ngOnInit(): void {
+   const localUser = JSON.parse(localStorage.getItem('user'));
+   this.currId = localUser.uid;
+ }
+ 
+ getLastMessage(messages: Message[]): string {
+   if (messages.length === 0) {
+     return '';
+   }
+ 
+   const lastMessage = messages[messages.length - 1];
+   return this.currId === lastMessage.senderId
+     ? lastMessage.text
+     : lastMessage.translatedText;
+ }
+ 
+ onSelect(chat: Chat) {
+   this.changeChat.emit(chat);
+ }
 }
+ 
+
