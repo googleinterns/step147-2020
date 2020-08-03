d0b0e6c05c4e65ab9f62953d47677e3ed85fb66b
mhmshatry@gmail.com <mhmshatry@gmail.com>
2020-08-03 20:17:09 +0000

    Update login component workflow

--- src/main/ngapp/src/app/login/login.component.ts
+++ src/main/ngapp/src/app/login/login.component.ts
@@ -1,59 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import * as firebase from 'firebase/app';
-
-
+ 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+ selector: 'app-login',
+ templateUrl: './login.component.html',
+ styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  loginAwait = false;
-  error: any;
-  errorPresent: boolean = false;
-  constructor(public authService: AuthService, private router: Router, public http: HttpClient) {}
-
-  ngOnInit(): void {}
-
-  login(email: string, password: string): void {
-    this.loginAwait = true;
-    this.authService
-      .login(email, password)
-      .then((res: any) => {
-        this.loginAwait = false;
-        this.router.navigate(['/chat']);
-      }) 
-      .catch((error: any) => {
-        this.loginAwait = false;
-        this.errorPresent = true;
-        this.error = error.message;
-      });
-  }
-
-  onClose() {
-    this.errorPresent = false;
-  }
-
-  routerSplit(res: any) {
-    if (res.additionalUserInfo.isNewUser) {
-      this.router.navigate(['/select-language']);
-    } else {
-      this.router.navigate(['/chat']);
-    }
-  }
-
-  loginWithGoogle() {
-    this.authService
-      .googleAuth()
-      .then((res) => {
-        this.routerSplit(res);
-      })
-      .catch((error: any) => {
-        this.errorPresent = true;
-        this.error = error.message;
-      });
-  }
+ loginAwait = false;
+ error: any;
+ errorPresent = false;
+ constructor(public authService: AuthService, private router: Router) {}
+ 
+ ngOnInit(): void {}
+ 
+ login(email: string, password: string): void {
+   this.loginAwait = true;
+   this.authService
+     .login(email, password)
+     .then((res: any) => {
+       this.loginAwait = false;
+       this.router.navigate(['/chat']);
+     })
+     .catch((error: any) => {
+       this.loginAwait = false;
+       this.errorPresent = true;
+       this.error = error.message;
+     });
+ }
+ 
+ onClose() {
+   this.errorPresent = false;
+ }
+ 
+ routerSplit(res: any) {
+   if (res.additionalUserInfo.isNewUser) {
+     this.router.navigate(['/select-language']);
+   } else {
+     this.router.navigate(['/chat']);
+   }
+ }
+ 
+ loginWithGoogle() {
+   this.authService
+     .googleAuth()
+     .then((res) => {
+       this.routerSplit(res);
+     })
+     .catch((error: any) => {
+       this.errorPresent = true;
+       this.error = error.message;
+     });
+ }
 }
