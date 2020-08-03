8a391e7b1094967ec03ed54a4dc8be8a7f9577e9
mhmshatry@gmail.com <mhmshatry@gmail.com>
2020-08-03 20:09:34 +0000

    Update angular styling

--- src/main/ngapp/src/app/app.module.ts
+++ src/main/ngapp/src/app/app.module.ts
@@ -4,14 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { AutocompleteLibModule } from 'angular-ng-autocomplete';
-
+ 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-
-import { ChangeUserInfoComponent } from './change-user-info/change-user-info.component';
+ 
 import { ChatBarComponent } from './chat-bar/chat-bar.component';
 import { ChatSectionComponent } from './chat-section/chat-section.component';
 import { LoginComponent } from './login/login.component';
@@ -22,87 +21,86 @@ import { SelectLanguageComponent } from './select-language/select-language.compo
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import {
-  NbThemeModule,
-  NbLayoutModule,
-  NbCardModule,
-  NbSearchModule,
-  NbUserModule,
-  NbListModule,
-  NbChatModule,
-  NbSpinnerModule,
-  NbInputModule,
-  NbButtonModule,
-  NbAlertModule,
-  NbIconModule,
-  NbSelectModule,
-  NbActionsModule,
-  NbContextMenuModule,
-  NbMenuModule,
-  NbDialogModule,
-  NbAutocompleteModule,
-  NbStepperModule,
-  NbBadgeModule,
-  NbToastrModule,
+ NbThemeModule,
+ NbLayoutModule,
+ NbCardModule,
+ NbSearchModule,
+ NbUserModule,
+ NbListModule,
+ NbChatModule,
+ NbSpinnerModule,
+ NbInputModule,
+ NbButtonModule,
+ NbAlertModule,
+ NbIconModule,
+ NbSelectModule,
+ NbActionsModule,
+ NbContextMenuModule,
+ NbMenuModule,
+ NbDialogModule,
+ NbAutocompleteModule,
+ NbStepperModule,
+ NbBadgeModule,
+ NbToastrModule,
 } from '@nebular/theme';
-
+ 
 // My web app's Firebase configuration.
 const firebaseConfig = {
- apiKey: "AIzaSyCtf7l7RL8_6H_B-fAlo5E8xJDZf9c2J2M",
-  authDomain: "team147-step2020.firebaseapp.com",
-  databaseURL: "https://team147-step2020.firebaseio.com",
-  projectId: "team147-step2020",
-  storageBucket: "team147-step2020.appspot.com",
-  messagingSenderId: "656351090386",
-  appId: "1:656351090386:web:92adc8b6ca7ef1393aff0b",
-  measurementId: "G-25QMK9RGPP"
+apiKey: "AIzaSyCtf7l7RL8_6H_B-fAlo5E8xJDZf9c2J2M",
+ authDomain: "team147-step2020.firebaseapp.com",
+ databaseURL: "https://team147-step2020.firebaseio.com",
+ projectId: "team147-step2020",
+ storageBucket: "team147-step2020.appspot.com",
+ messagingSenderId: "656351090386",
+ appId: "1:656351090386:web:92adc8b6ca7ef1393aff0b",
+ measurementId: "G-25QMK9RGPP"
 };
-
+ 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ChatBarComponent,
-    ChatSectionComponent,
-    LoginComponent,
-    RegisterComponent,
-    MainComponent,
-    ConfirmEqualValidatorDirective,
-    ChangeUserInfoComponent,
-    SelectLanguageComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireAuthModule,
-    RouterModule,
-    HttpClientModule,
-    AutocompleteLibModule,
-    BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'default' }),
-    NbLayoutModule,
-    NbEvaIconsModule,
-    NbCardModule,
-    NbSearchModule,
-    NbUserModule,
-    NbListModule,
-    NbChatModule,
-    NbSpinnerModule,
-    NbInputModule,
-    NbButtonModule,
-    NbAlertModule,
-    NbIconModule,
-    NbSelectModule,
-    NbActionsModule,
-    NbContextMenuModule,
-    NbMenuModule.forRoot(),
-    NbDialogModule.forRoot(),
-    NbToastrModule.forRoot(),
-    NbAutocompleteModule,
-    NbStepperModule,
-    NbBadgeModule
-  ],
-  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
-  bootstrap: [AppComponent],
+ declarations: [
+   AppComponent,
+   ChatBarComponent,
+   ChatSectionComponent,
+   LoginComponent,
+   RegisterComponent,
+   MainComponent,
+   ConfirmEqualValidatorDirective,
+   SelectLanguageComponent,
+ ],
+ imports: [
+   BrowserModule,
+   AppRoutingModule,
+   FormsModule,
+   AngularFireModule.initializeApp(firebaseConfig),
+   AngularFireAuthModule,
+   RouterModule,
+   HttpClientModule,
+   AutocompleteLibModule,
+   BrowserAnimationsModule,
+   NbThemeModule.forRoot({ name: 'default' }),
+   NbLayoutModule,
+   NbEvaIconsModule,
+   NbCardModule,
+   NbSearchModule,
+   NbUserModule,
+   NbListModule,
+   NbChatModule,
+   NbSpinnerModule,
+   NbInputModule,
+   NbButtonModule,
+   NbAlertModule,
+   NbIconModule,
+   NbSelectModule,
+   NbActionsModule,
+   NbContextMenuModule,
+   NbMenuModule.forRoot(),
+   NbDialogModule.forRoot(),
+   NbToastrModule.forRoot(),
+   NbAutocompleteModule,
+   NbStepperModule,
+   NbBadgeModule,
+ ],
+ providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+ bootstrap: [AppComponent],
 })
 export class AppModule {}
