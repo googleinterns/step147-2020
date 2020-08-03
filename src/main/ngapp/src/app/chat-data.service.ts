a7038541ae738c4c0b5a50b794bdc74d1dc08f91
mhmshatry@gmail.com <mhmshatry@gmail.com>
2020-08-03 20:16:45 +0000

    Update headers fetch component

--- src/main/ngapp/src/app/chat-data.service.ts
+++ src/main/ngapp/src/app/chat-data.service.ts
@@ -2,122 +2,102 @@ import { Injectable } from '@angular/core';
 import { Message } from './models/message';
 import { Chatroom } from './models/chatroom';
 import { User } from './models/user';
-import { Post } from './models/post';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth/auth.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { environment } from './../environments/environment';
-import * as firebase from 'firebase/app';
-
-
+ 
 @Injectable({
-  providedIn: 'root',
+ providedIn: 'root',
 })
 export class ChatDataService {
-
-  constructor(private authService: AuthService, private http: HttpClient) {}
-
-  // Get the object associated with the currrent user.
-  getUser(): Observable<User> {
-    const localUser = JSON.parse(localStorage.getItem('user'));
-    const httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json',
-            'X-token': localStorage.idToken
-        }),
-    };
-    return this.http.get<User>('/user?userId=' + localUser.uid, httpOptions);
-  }
-
-  // Create a new user object for the user.
-  addUser(user: User): Observable<any> {
-    const httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json',
-            'X-token': localStorage.idToken
-        }),
-    };
-    return this.http.post<User>('/user', user, httpOptions);
-  }
-
-  updateLanguage(user: User): Observable<any> {
-    const httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json',
-            'X-token': localStorage.idToken
-        }),
-    };
-    return this.http.put<User>('/user', user, httpOptions);
-  }
-
-  // Get a list of all the users in our service.
-  getUsers(): Observable<User[]> {
-    const localUser = JSON.parse(localStorage.getItem('user'));
-    const httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json',
-            'X-token': localStorage.idToken
-        }),
-    };
-    // Adding httpOptions to the url.
-    return this.http.get<User[]>('/users?userId=' + localUser.uid, httpOptions);
-  }
-
-  // Get messages between two users.
-  getMessages(): Observable<Message[]> {
-    const localUser = JSON.parse(localStorage.getItem('user'));
-    const httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json',
-            'X-token': localStorage.idToken
-        }),
-    };
-    const url: string = '/messages?userId=' + localUser.uid;
-    return this.http.get<Message[]>(url, httpOptions);
-  }
-
-  // Get a chatroom for a recepient.
-  getChatroom(recipient: string): Observable<Chatroom[]> {
-    const localUser = JSON.parse(localStorage.getItem('user'));
-    const httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json',
-            'X-token': localStorage.idToken
-        }),
-    };
-    // Adding httpOptions to the url.
-    const url: string =
-      '/getChatroom?userId=' + localUser.uid + '&recipientId=' + recipient;
-    return this.http.get<Chatroom[]>(url, httpOptions);
-  }
-
-  // Get all chatrooms.
-  getChatrooms(): Observable<Chatroom[]> {
-    const localUser = JSON.parse(localStorage.getItem('user'));
-    const httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json',
-            'X-token': localStorage.idToken
-        }),
-    };
-    const url: string = '/chatrooms?userId=' + localUser.uid;
-    return this.http.get<Chatroom[]>(url, httpOptions);
-  }
-
-  // Add message in a new chatroom.
-  addMessage(input: Post): Observable<any> {
-    const httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json',
-            'X-token': localStorage.idToken
-        }),
-    };
-    return this.http.post<Post>('/messages', input, httpOptions);
-  }
-
-  // Logout of the service.
-  logout() {
-    this.authService.logout();
-  }
+ constructor(private authService: AuthService, private http: HttpClient) {}
+ 
+ getHeader(): HttpHeaders {
+   return new HttpHeaders({
+     'Content-Type': 'application/json',
+     'X-token': localStorage.idToken,
+   });
+ }
+ 
+ // Get the object associated with the current user.
+ getUser(): Observable<User> {
+   const localUser = JSON.parse(localStorage.getItem('user'));
+   const httpHeaders = this.getHeader();
+   const httpOptions = {
+     headers: httpHeaders,
+   };
+ 
+   return this.http.get<User>('/user?userId=' + localUser.uid, httpOptions);
+ }
+ 
+ // Create a new user object for the user.
+ addUser(user: User): Observable<any> {
+   const httpHeaders = this.getHeader();
+   const httpOptions = {
+     headers: httpHeaders,
+   };
+ 
+   return this.http.post<User>('/user', user, httpOptions);
+ }
+ 
+ updateLanguage(user: User): Observable<any> {
+   const httpHeaders = this.getHeader();
+   const httpOptions = {
+     headers: httpHeaders,
+   };
+ 
+   return this.http.put<User>('/user', user, httpOptions);
+ }
+ 
+ // Get a list of all the users in our service.
+ getUsers(): Observable<User[]> {
+   const localUser = JSON.parse(localStorage.getItem('user'));
+   const httpHeaders = this.getHeader();
+   const httpOptions = {
+     headers: httpHeaders,
+   };
+   // Adding httpOptions to the url.
+   return this.http.get<User[]>('/users?userId=' + localUser.uid, httpOptions);
+ }
+ 
+ // Get messages between two users.
+ getMessages(): Observable<Message[]> {
+   const localUser = JSON.parse(localStorage.getItem('user'));
+   const httpHeaders = this.getHeader();
+   const httpOptions = {
+     headers: httpHeaders,
+   };
+ 
+   const url: string = '/messages?userId=' + localUser.uid;
+   return this.http.get<Message[]>(url, httpOptions);
+ }
+ 
+ // Get all chatrooms.
+ getChatrooms(): Observable<Chatroom[]> {
+   const localUser = JSON.parse(localStorage.getItem('user'));
+   const httpHeaders = this.getHeader();
+   const httpOptions = {
+     headers: httpHeaders,
+   };
+ 
+   const url: string = '/chatrooms?userId=' + localUser.uid;
+   return this.http.get<Chatroom[]>(url, httpOptions);
+ }
+ 
+ // Add message in a new chatroom.
+ addMessage(input: Message): Observable<any> {
+   const httpHeaders = this.getHeader();
+   const httpOptions = {
+     headers: httpHeaders,
+   };
+ 
+   return this.http.post<Message>('/messages', input, httpOptions);
+ }
+ 
+ // Logout of the service.
+ logout() {
+   this.authService.logout();
+ }
 }
+ 
+
