677e302aef697e60241259afa2cc43fb53a74299
mhmshatry@gmail.com <mhmshatry@gmail.com>
2020-08-03 20:07:44 +0000

    Update classes to include set entity class

--- src/main/java/com/google/sps/servlets/Message.java
+++ src/main/java/com/google/sps/servlets/Message.java
@@ -1,36 +1,48 @@
 package com.google.sps.servlets;
 import com.google.appengine.api.datastore.Entity;
-
+import com.google.appengine.api.datastore.DatastoreService;
+ 
 public class Message {
-    public String messageId;
-    public String chatroomId;
-    public String text;
-    public String translatedText;
-    public String senderId;
-    public String recipientId;
-    public Long timestamp;
-
-    public Message(String newMessageId, String newChatroomId, String newText, String newTranslatedText, String newSenderId, String newRecipientId, Long newTimestamp) {
-        this.messageId = newMessageId;
-        this.chatroomId = newChatroomId;
-        this.text = newText;
-        this.translatedText = newTranslatedText;
-        this.senderId = newSenderId;
-        this.recipientId = newRecipientId;
-        this.timestamp = newTimestamp;
-    }
-
-    public Message(Entity entity) {
-        this.messageId = (String) entity.getProperty("messageId");
-        this.chatroomId = (String) entity.getProperty("chatroomId");
-        this.text = (String) entity.getProperty("text");
-        this.translatedText = (String) entity.getProperty("translatedText");
-        this.senderId = (String) entity.getProperty("senderId");
-        this.recipientId = (String) entity.getProperty("recipientId");
-        this.timestamp = (Long) entity.getProperty("timestamp");
-    }
-
-    public String getChatroom(){
-        return chatroomId;
-    }
+   public String messageId;
+   public String chatroomId;
+   public String text;
+   public String translatedText;
+   public String senderId;
+   public String recipientId;
+   public Long timestamp;
+ 
+   public Message(String newMessageId, String newChatroomId, String newText, String newTranslatedText, String newSenderId, String newRecipientId, Long newTimestamp) {
+       this.messageId = newMessageId;
+       this.chatroomId = newChatroomId;
+       this.text = newText;
+       this.translatedText = newTranslatedText;
+       this.senderId = newSenderId;
+       this.recipientId = newRecipientId;
+       this.timestamp = newTimestamp;
+   }
+ 
+   public Message(Entity entity) {
+       this.messageId = (String) entity.getProperty("messageId");
+       this.chatroomId = (String) entity.getProperty("chatroomId");
+       this.text = (String) entity.getProperty("text");
+       this.translatedText = (String) entity.getProperty("translatedText");
+       this.senderId = (String) entity.getProperty("senderId");
+       this.recipientId = (String) entity.getProperty("recipientId");
+       this.timestamp = (Long) entity.getProperty("timestamp");
+   }
+ 
+   public setEntity(){
+       Entity newMessage = new Entity("message");
+       newMessage.setProperty("messageId", this.messageId);
+       newMessage.setProperty("chatroomId", this.chatroomId);
+       newMessage.setProperty("text", this.text);
+       newMessage.setProperty("translatedText", this.translatedText);
+       newMessage.setProperty("senderId", this.senderId);
+       newMessage.setProperty("recipientId", this.recipientId);
+       newMessage.setProperty("timestamp", this.timestamp);
+ 
+       DatastoreServiceFactory.getDatastoreService().put(newMessage);
+   }
+ 
 }
+
--- src/main/java/com/google/sps/servlets/User.java
+++ src/main/java/com/google/sps/servlets/User.java
@@ -1,23 +1,35 @@
 package com.google.sps.servlets;
 import com.google.appengine.api.datastore.Entity;
-
+import com.google.appengine.api.datastore.DatastoreService;
+ 
 public class User {
-    public String userId;
-    public String name;
-    public String email;
-    public String language;
-
-    public User(String userID, String name, String email, String language) {
-        this.userId = userID;
-        this.name = name;
-        this.email = email;
-        this.language = language;
-    }
-
-    public User(Entity entity) {
-        this.userId = (String) entity.getProperty("userId");
-        this.name = (String) entity.getProperty("name");
-        this.email = (String) entity.getProperty("email");
-        this.language = (String) entity.getProperty("language");
-    }
+   public String userId;
+   public String name;
+   public String email;
+   public String language;
+ 
+   public User(String userID, String name, String email, String language) {
+       this.userId = userID;
+       this.name = name;
+       this.email = email;
+       this.language = language;
+   }
+ 
+   public User(Entity entity) {
+       this.userId = (String) entity.getProperty("userId");
+       this.name = (String) entity.getProperty("name");
+       this.email = (String) entity.getProperty("email");
+       this.language = (String) entity.getProperty("language");
+   }
+ 
+   public setEntity(){
+       Entity newUser = new Entity("user", this.userId);
+       newUser.setProperty("userId", this.userId);
+       newUser.setProperty("name", this.name);
+       newUser.setProperty("email", this.email);
+       newUser.setProperty("language", this.language);
+      
+       DatastoreServiceFactory.getDatastoreService().put(newUser);
+   }
 }
+
